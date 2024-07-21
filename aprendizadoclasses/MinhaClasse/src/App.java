46d6f7f4a4ef64d468fbae7014c094d72a491c3c
Jairo <jairomunier@gmail.com>
2024-07-20 21:45:19 -0300

    segunda alteração

--- aprendizadoclasses/MinhaClasse/src/App.java
+++ aprendizadoclasses/MinhaClasse/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!tudo bem?");
+        System.out.println("Hello, World!tudo bem? porque eu estou otimo");
     }
 }
