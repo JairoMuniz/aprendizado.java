2009a1eed0a59e3230b026c56e840d6ad793058d
Jairo <jairomunier@gmail.com>
2024-07-20 21:49:16 -0300

    terceira

--- aprendizadoclasses/MinhaClasse/src/App.java
+++ aprendizadoclasses/MinhaClasse/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!tudo bem? porque eu estou otimo");
+        System.out.println("Hello, World!tudo bem? porque eu estou otimo kk");
     }
 }
